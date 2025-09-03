85b145c6f94fc9bfb7b51fb160f7a1dd51a56b69
unknown <mendikerezbekova02@gmail.com>
2025-09-03 17:55:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//условия (if else, swich), операторы
+
+//boolean true, false
+
+// ==, ===
+
+// let num = 5 //string
+// let num2 = 5 //number
+
+// console.log(num == num2);
+// console.log(num === num2);
+// < > <= >= !== ===
+//логические операторы
+
+// && (и - жана), || (или - же), ??
+
+// if,else
+	// let age =19
+
+let ball = 100
+if (ball<40) {
+	console.log('2-tupe');
+	}
+	let 
